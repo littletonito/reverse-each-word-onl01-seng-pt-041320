f9956667508ac11da2e6a7880b15db46b9ee5f02
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-24 19:55:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,6 +7,3 @@ def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
-# def reverse_each_word(sentence)
-#   sentence.split.collect {|word| word.reverse}.join(" ")
-# end
